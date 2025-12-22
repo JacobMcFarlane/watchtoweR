e3e8ce538285568b2fc5741f639aecf2e1ccb110
Jacob McFarlane <jacob@jakenow.net>
2025-12-22 09:23:06 -1000

    implement write if no snapshot

--- R/watch.R
+++ R/watch.R
@@ -12,15 +12,42 @@
 #' @export 
 watch_df <- function(
   df, 
-  watch_name,
+  watch_name = NULL,
   watch_dir = "tower_snaps"
 ){
   
+  if (is.null(watch_name)) {
+    watch_name <- rlang::as_name(rlang::enquo(df))
+    cli::cli_alert("No watch name provided, using {watch_name}")
+  }
 
+  if (!fs::dir_exists(watch_dir)) {
+    if (rlang::is_interactive()) {
+      res <- menu(
+        c("Yes", "No"),
+        title="Passed watch dir does not exist, do you want to create it?"
+      )
 
-  # Watch dir df
-  regex = glue::glue("^[:digit:]{4}-[:digit:]{2}-[:digit:]{2}T[:digit:]{6}_{watch_name}\\.rda$")
-  files = fs::dir_ls(watch_dir, pattern = regex)
+      if (res == 1) {
+        fs::dir_create(watch_dir, recurse = TRUE)
+      } else {
+        cli::cli_abort("{watch_dir} must exist")
+      }
+
+    } else {
+      cli::cli_abort("{watch_dir} must exist if `watch_df` not run interactively")
+    }
+  }
+
+  matches <- list_watch_df_snapshots(watch_name, watch_dir)
+
+  if (length(matches) == 0) {
+    cli::cli_alert("No match found for {watch_name} in {watch_dir}, creating new snapshot")
+    readr::write_rds(
+       x = df,
+       fs::path_join(c(watch_dir, build_watch_filename(watch_name)))
+    )
+  }
 
   # Check if watch_name is in watch_dir
     # If not - alert and save a new one 
--- tests/testthat/test-watch.R
+++ tests/testthat/test-watch.R
@@ -1,12 +1,11 @@
 test_that("Basic watch_df functionality works as expected", {
-  # Alert and save if no matches for string in testdir
   basic_df <- watch_fixtures()$base_df
   watch_folder <- withr::local_tempdir()
 
-
   expect_message(
-    watch(basic_df,
-          watch_folder),
+    watch_df(basic_df,
+            watch_dir = watch_folder),
     regexp = "No match found for basic_df in"
   )
+
 })
